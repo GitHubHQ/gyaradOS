59caf640ecd7b7798b3500768856d049ed6b4b73
Tanishq Dubey <tanishq.dubey@gmail.com>
2016-03-11 15:00:38 -0600

    Added IRQ defines

--- student-distrib/drivers/keyboard.c
+++ student-distrib/drivers/keyboard.c
@@ -1,11 +1,11 @@
+/* keyboard.c - Functions to interact with the keyboard
+ */
 
 #include "lib.h"
 
-#define KEYBOARD_D_PORT 0x60
-
 /**
  * Reads a scan code from the keyboard port
- * @return  The scan code sent from the keyboard
+ * @return  The scan code sent from the keyboard (Not ASCII)
  */
 unsigned char kbrd_read_scan_code(void) {
 	return inb(KEYBOARD_D_PORT);
--- student-distrib/drivers/keyboard.h
+++ student-distrib/drivers/keyboard.h
@@ -1,6 +1,14 @@
+/*
+ * keyboard.h - Defines used in interactions with the keyboard
+ */
+
 #ifndef _KEYBOARD_H
 #define _KEYBOARD_H
 
-unsigned char kbrd_read_scan_code(void)
+// Data I/O port for the keyboard
+#define KEYBOARD_D_PORT 0x60
+
+// Reads a scan code from the keyboard port
+unsigned char kbrd_read_scan_code(void);
 
 #endif  /* _KEYBOARD_H */
