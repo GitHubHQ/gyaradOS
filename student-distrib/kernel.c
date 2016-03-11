87465aee175f0b72800ece020f333d287cfd4a83
Rohan Mathur <rohan@rmathur.com>
2016-03-11 17:50:10 -0600

    Enable interrupts
    This might help
    
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -149,13 +149,14 @@ entry (unsigned long magic, unsigned long addr)
 
 	/* Initialize devices, memory, filesystem, enable device interrupts on the
 	 * PIC, any other initialization stuff... */
+	enable_irq(1);
 
 	/* Enable interrupts */
 	/* Do not enable the following until after you have set up your
 	 * IDT correctly otherwise QEMU will triple fault and simple close
 	 * without showing you any output */
-	/*printf("Enabling Interrupts\n");
-	sti();*/
+	printf("Enabling Interrupts\n");
+	sti();
 
 	/* Execute the first program (`shell') ... */
 
