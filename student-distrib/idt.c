f4b3c0f6c7b4944584d663fa3c62e7d4df3de590
Rohan Mathur <rohan@rmathur.com>
2016-03-11 12:35:24 -0600

    Implement our BSOD
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -3,97 +3,127 @@
 
 // 0 - Division by zero exception
 void divide_by_zero_except() {
-
+    printf("EXCEPTION: Divide by zero!\n");
+    while(1);
 }
 
 // 1 - Debug exception
 void debug_except() {
-
+    printf("EXCEPTION: Debug!\n");
+    while(1);
 }
 
 // 2 - Non maskable interrupt
 void nmi_except() {
-
+    printf("EXCEPTION: Non-maskable interrupt!\n");
+    while(1);
 }
 
 // 3 - Breakpoint exception
 void breakpoint_except() {
-
+    printf("EXCEPTION: Breakpoint!\n");
+    while(1);
 }
 
 // 4 - 'Into detected overflow'
 void overflow_except() {
-
+    printf("EXCEPTION: Into detected overflow!\n");
+    while(1);
 }
 
 // 5 - Out of bounds exception
 void oob_except() {
-
+    printf("EXCEPTION: Out of bounds!\n");
+    while(1);
 }
 
 // 6 - Invalid opcode exception
 void opcode_except() {
-
+    printf("EXCEPTION: Invalid opcode!\n");
+    while(1);
 }
 
 // 7 - No coprocessor exception
 void no_coproc_except() {
-
+    printf("EXCEPTION: No coprocessor!\n");
+    while(1);
 }
 
 // 8 - Double fault (pushes an error code)
 void double_fault_except() {
     // TODO push error code
+
+    printf("EXCEPTION: Double fault!\n");
+    while(1);
 }
 
 // 9 - Coprocessor segment overrun
 void coproc_seg_overrun_except() {
-
+    printf("EXCEPTION: Coprocessor segment overrun!\n");
+    while(1);
 }
 
 // 10 - Bad TSS (pushes an error code)
 void tss_except() {
     // TODO push error code
+
+    printf("EXCEPTION: Bad TSS!\n");
+    while(1);
 }
 
 // 11 - Segment not present (pushes an error code)
 void segment_exept() {
     // TODO push error code
+
+    printf("EXCEPTION: Segment not present!\n");
+    while(1);
 }
 
 // 12 - Stack fault (pushes an error code)
 void stack_except() {
     // TODO push error code
+
+    printf("EXCEPTION: Stack fault!\n");
+    while(1);
 }
 
 // 13 - General protection fault (pushes an error code)
 void general_protec_except() {
     // TODO push error code
+
+    printf("EXCEPTION: General protection fault!\n");
+    while(1);
 }
 
 // 14 - Page fault (pushes an error code)
 void page_fault_except() {
     // TODO push error code
+
+    printf("EXCEPTION: Page fault!\n");
+    while(1);
 }
 
 // 15 - Unknown interrupt exception
 void unknown_interr_except() {
-
+    printf("EXCEPTION: Unknown interrupt!\n");
+    while(1);
 }
 
 // 16 - Coprocessor fault
 void coproc_except() {
-
+    printf("EXCEPTION: Coprocessor fault!\n");
+    while(1);
 }
 
 // 17 - Alignment check exception
 void align_except() {
-
+    printf("EXCEPTION: Alignment check!\n");
+    while(1);
 }
 
 // 18 - Machine check exception
 void machine_chk_except() {
-
+    printf("EXCEPTION: Machine check!\n");
 }
 
 void init_idt() {
