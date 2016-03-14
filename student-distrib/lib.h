d8cc7376158b3a99d3d62bb4238b26b4e93fa475
Tanishq Dubey <tanishq.dubey@gmail.com>
2016-03-13 21:13:55 -0500

    Fixed implicit decleration

--- student-distrib/lib.h
+++ student-distrib/lib.h
@@ -28,6 +28,9 @@ int8_t* strncpy(int8_t* dest, const int8_t*src, uint32_t n);
 int32_t bad_userspace_addr(const void* addr, int32_t len);
 int32_t safe_strncpy(int8_t* dest, const int8_t* src, int32_t n);
 
+/* RTC test function */
+void test_interrupts(void);
+
 /* Port read functions */
 /* Inb reads a byte and returns its value as a zero-extended 32-bit
  * unsigned int */
