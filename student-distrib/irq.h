eda40e0a5fd2def17a3fc1e4af694010af1afb60
Rohan Mathur <rohan@rmathur.com>
2016-03-13 22:29:26 -0500

    Finish commenting my functions
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- student-distrib/irq.h
+++ student-distrib/irq.h
@@ -2,7 +2,10 @@
 #ifndef IRQ_H
 #define IRQ_H
 
+// handler for keyboard interrupts
 extern void key_irq();
+
+// handler for RTC interrupts
 extern void rtc_irq();
 
 #endif /* IRQ_H*/
--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,6 +1,7 @@
 
 #include "keyboard.h"
 
+// array of ASCII codes, indexed by the corresponding characters scan code
 uint8_t code_to_ascii[] = {
     ASCII_NULL_CHAR, ASCII_PLACEHOLDER, ASCII_1, ASCII_2, ASCII_3, ASCII_4, ASCII_5, ASCII_6, ASCII_7, ASCII_8, ASCII_9, ASCII_0, ASCII_HYPHEN, ASCII_EQUALS, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_Q, ASCII_W, ASCII_E, ASCII_R, ASCII_T, ASCII_Y, ASCII_U, ASCII_I, ASCII_O, ASCII_P, ASCII_LEFT_BRACKET, ASCII_RIGHT_BRACKET, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_A, ASCII_S, ASCII_D, ASCII_F, ASCII_G, ASCII_H, ASCII_J, ASCII_K, ASCII_L, ASCII_SEMICOLON, ASCII_TICK, ASCII_ASTERISK, ASCII_PLACEHOLDER, ASCII_BACKWARDS_SLASH, ASCII_Z, ASCII_X, ASCII_C, ASCII_V, ASCII_B, ASCII_N, ASCII_M, ASCII_COMMA, ASCII_PERIOD, ASCII_FORWARDS_SLASH, ASCII_PLACEHOLDER, ASCII_KP_ASTERISK, ASCII_PLACEHOLDER, ASCII_SPACE, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER, ASCII_KP_7, ASCII_KP_8, ASCII_KP_9, ASCII_KP_HYPHEN, ASCII_KP_4, ASCII_KP_5, ASCII_KP_6, ASCII_KP_PLUS, ASCII_KP_1, ASCII_KP_2, ASCII_KP_3, ASCII_KP_0, ASCII_KP_PERIOD, ASCII_NULL_CHAR, ASCII_NULL_CHAR, ASCII_NULL_CHAR, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER
 };
@@ -8,29 +9,30 @@ uint8_t code_to_ascii[] = {
 // int special_key = 0;
 
 /**
- * Handles the keypress interrupt from the keyboard
+ * handle_keypress()
+ * 
+ * Description: Prints the key pressed by the keyboard to the screen
+ *
+ * Inputs: None (keycode is read from the keyboard port)
+ * Outputs: Prints the corresponding character to the screen
+ * Returns: None
  */
 void handle_keypress() {
     cli();
 
     uint8_t key_code = inb(KEYBOARD_D_PORT);
 
-    // if(key_code == SPECIAL_KEY) {
-    //     special_key = 1;
-    // } else if (special_key) {
-    //     // handle special key from special key table
-    // } else {
-        if(key_code > 0x58) {
-            i8259_ioctl(I8259_SEND_EOI, IRQ_KEYBOARD_CTRL);
-            sti();
-            return;
-        }
+    if(key_code > 0x58) {
+        i8259_ioctl(I8259_SEND_EOI, IRQ_KEYBOARD_CTRL);
+        sti();
+        return;
+    }
 
-        uint8_t key_ascii = code_to_ascii[key_code];
-        if(key_ascii != ASCII_PLACEHOLDER) {
-            printf("%c", code_to_ascii[key_code]);
-        }
-    // }
+    uint8_t key_ascii = code_to_ascii[key_code];
+    if(key_ascii != ASCII_PLACEHOLDER) {
+        printf("%c", code_to_ascii[key_code]);
+    }
+
     i8259_ioctl(I8259_SEND_EOI, IRQ_KEYBOARD_CTRL);
     sti();
 }
