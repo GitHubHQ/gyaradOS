b378ff40991ce027e9ad1424558fb259427b4b39
Rohan Mathur <rohan@rmathur.com>
2016-03-20 14:10:39 -0400

    Fix compile errors with file skeleton
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- student-distrib/files.c
+++ student-distrib/files.c
@@ -1,13 +1,13 @@
 #include "files.h"
 
-int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry){
-
+int32_t read_dentry_by_name (const uint8_t* fname, struct dentry_t * dentry) {
+    return 0;
 }
 
-int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry){
-
+int32_t read_dentry_by_index (uint32_t index, struct dentry_t * dentry) {
+    return 0;
 }
 
-int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
-
+int32_t read_data (uint32_t inode, uint32_t offset, uint8_t * buf, uint32_t length) {
+    return 0;
 }
--- student-distrib/files.h
+++ student-distrib/files.h
@@ -1,21 +1,8 @@
-#ifdef _FILES_H
+#ifndef _FILES_H
 #define _FILES_H
 
-#include "types.h"
-#include "lib.h"
-
-struct boot_block {
-	uint32_t dir_entries;
-	uint32_t inodes;
-	uint32_t data_blocks;
-	uint32_t blocks_reserved[52];
-	dentry_t d_entry[63];
-};
-
-struct i_node {
-	uint32_t file_size;
-	uint32_t blocks[1023];
-};
+#include "libs/types.h"
+#include "libs/lib.h"
 
 struct dentry_t {
 	uint8_t file_name[32];
@@ -24,8 +11,21 @@ struct dentry_t {
 	uint32_t blocks_reserved[24];
 };
 
-int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry);
-int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
-int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
+struct i_node {
+	uint32_t file_size;
+	uint32_t blocks[1023];
+};
+
+struct boot_block {
+	uint32_t dir_entries;
+	uint32_t inodes;
+	uint32_t data_blocks;
+	uint32_t blocks_reserved[52];
+	struct dentry_t d_entry[63];
+};
+
+int32_t read_dentry_by_name (const uint8_t* fname, struct dentry_t * dentry);
+int32_t read_dentry_by_index (uint32_t index, struct dentry_t * dentry);
+int32_t read_data (uint32_t inode, uint32_t offset, uint8_t * buf, uint32_t length);
 
 #endif
