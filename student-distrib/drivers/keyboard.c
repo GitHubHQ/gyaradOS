2ff3310a8fd2291c3d745c79f8d12880374a8f28
Rohan Mathur <rohan@rmathur.com>
2016-03-21 17:41:32 -0400

    Keyboard bugfixes
    * handle holding down caps correctly
    * Fix the mapping of `/~ and '/"
    
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- student-distrib/drivers/keyboard.c
+++ student-distrib/drivers/keyboard.c
@@ -9,7 +9,7 @@ uint8_t code_to_ascii[] = {
     ASCII_E, ASCII_R, ASCII_T, ASCII_Y, ASCII_U, ASCII_I, ASCII_O, ASCII_P,
     ASCII_LEFT_BRACKET, ASCII_RIGHT_BRACKET, ASCII_PLACEHOLDER, ASCII_PLACEHOLDER,
     ASCII_A, ASCII_S, ASCII_D, ASCII_F, ASCII_G, ASCII_H, ASCII_J, ASCII_K,
-    ASCII_L, ASCII_SEMICOLON, ASCII_TICK, ASCII_ASTERISK, ASCII_PLACEHOLDER,
+    ASCII_L, ASCII_SEMICOLON, '\'', '`', ASCII_PLACEHOLDER,
     ASCII_BACKWARDS_SLASH, ASCII_Z, ASCII_X, ASCII_C, ASCII_V, ASCII_B, ASCII_N,
     ASCII_M, ASCII_COMMA, ASCII_PERIOD, ASCII_FORWARDS_SLASH, ASCII_PLACEHOLDER,
     ASCII_KP_ASTERISK, ASCII_PLACEHOLDER, ASCII_SPACE, ASCII_PLACEHOLDER,
@@ -30,7 +30,7 @@ uint8_t caps_ascii[] = {
     'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
     '{', '}', ASCII_PLACEHOLDER, ASCII_PLACEHOLDER,
     'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K',
-    'L', ':', '~', ASCII_ASTERISK, ASCII_PLACEHOLDER,
+    'L', ':', '"', '~', ASCII_PLACEHOLDER,
     '|', 'Z', 'X', 'C', 'V', 'B', 'N',
     'M', '<', '>', '?', ASCII_PLACEHOLDER,
     ASCII_KP_ASTERISK, ASCII_PLACEHOLDER, ASCII_SPACE, ASCII_PLACEHOLDER,
@@ -49,7 +49,10 @@ uint8_t num_chars_in_buf = 0;
 // variables designating status of special keys
 int cntrl_l_on = 0;
 int cntrl_r_on = 0;
+
 int caps_on = 0;
+int caps_btn_pressed = 0;
+
 int shift_l_on = 0;
 int shift_r_on = 0;
 
@@ -218,8 +221,11 @@ void handle_keypress() {
                     shift_r_on = 1;
                     break;
                 case KEY_MAKE_CAPS:
-                    caps_on = !caps_on;
-                    kbd_led_handling(caps_on, 0, 0);
+                    if(!caps_btn_pressed) {
+                        caps_on = !caps_on;
+                        kbd_led_handling(caps_on, 0, 0);
+                    }
+                    caps_btn_pressed = 1;
                     break;
                 case KEY_MAKE_ENTER:
                     handle_enter();
@@ -259,6 +265,9 @@ void handle_keypress() {
                     break;
                 case KEY_BREAK_R_SHIFT:
                     shift_r_on = 0;
+                    break;
+                case KEY_BREAK_CAPS:
+                    caps_btn_pressed = 0;
                     break;
                 default:
                     // we don't care about the rest of the breaks
