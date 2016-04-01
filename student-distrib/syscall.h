e65e9ac616645488cb122e68399a469552200a65
Rohan Mathur <rohan@rmathur.com>
2016-03-31 21:19:58 -0500

    Finish syscall asm wrapper
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- student-distrib/syscall.h
+++ student-distrib/syscall.h
@@ -4,15 +4,15 @@
 
 #include "libs/types.h"
 
-int32_t halt (uint8_t status);
-int32_t execute (const uint8_t * command);
-int32_t read (int32_t fd, void * buf, int32_t nbytes);
-int32_t write (int32_t fd, const void * buf, int32_t nbytes);
-int32_t open (const uint8_t * filename);
-int32_t close (int32_t fd);
-int32_t getargs (uint8_t * buf, int32_t nbytes);
-int32_t vidmap (uint8_t ** screen_start);
-int32_t set_handler (int32_t signum, void * handler_address);
-int32_t sigreturn (void);
+extern int32_t halt (uint8_t status);
+extern int32_t execute (const uint8_t * command);
+extern int32_t read (int32_t fd, void * buf, int32_t nbytes);
+extern int32_t write (int32_t fd, const void * buf, int32_t nbytes);
+extern int32_t open (const uint8_t * filename);
+extern int32_t close (int32_t fd);
+extern int32_t getargs (uint8_t * buf, int32_t nbytes);
+extern int32_t vidmap (uint8_t ** screen_start);
+extern int32_t set_handler (int32_t signum, void * handler_address);
+extern int32_t sigreturn (void);
 
 #endif /* _SYSCALL_H */
