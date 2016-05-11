3e5c990d96d52eb7793d2c0aa3c05b1c8de1b709
Rohan Mathur <rohan@rmathur.com>
2016-05-11 12:40:13 -0500

    Reset dev-tools branch
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- student-distrib/include/kernel/user.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef _USER_H
-#define _USER_H
-
-extern void jmp_usr_exec(uint32_t addr);
-
-#endif /* _USER_H*/
--- student-distrib/include/lib/math.h
+++
@@ -1,7 +0,0 @@
-#include <lib/lib.h>
-
-/* External Functions */
-float logBase2 (float x);
-float log (float x);
-int ceil(float x);
-int floor(float x);
